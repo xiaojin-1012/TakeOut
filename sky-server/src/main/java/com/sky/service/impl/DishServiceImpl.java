6164f56ac0f2fbb134ee3762a1777fcd3bdfb0a1
jinyuanpeng <2220450251@qq.com>
2024-04-01 20:09:18 +0800

    菜品分页查询功能代码

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,10 +41,22 @@ public class DishServiceImpl implements DishService {
         Long dishId = dish.getId();
         //口味表插入n条数据
         List<DishFlavor> flavors = dishDTO.getFlavors();
-       flavors.forEach(dishFlavor -> {
-           dishFlavor.setDishId(dishId);
-       });
+        flavors.forEach(dishFlavor -> {
+            dishFlavor.setDishId(dishId);
+        });
         dishFlavorMapper.insertBatch(flavors);
+    }
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }
 
